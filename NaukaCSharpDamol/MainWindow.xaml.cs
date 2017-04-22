832bc345af8673139b18ed3d230af37b7e80dc56
Damian Korneluk <damol9@wp.pl>
2017-04-23 01:26:13 +0200

    Update #3: Added registration, some bugfixes.

--- NaukaCSharpDamol/MainWindow.xaml.cs
+++ NaukaCSharpDamol/MainWindow.xaml.cs
@@ -1,12 +1,11 @@
-﻿#undef DEBUG
-//#define DEBUG
+﻿//#undef DEBUG
+#define DEBUG
 using System;
 using System.Windows;
 using System.Windows.Threading;
-using System.Data;
-using MySql.Data;
 using System.Threading;
 using MySql.Data.MySqlClient;
+//using System.IO;
 
 namespace NaukaCSharpDamol
 {
@@ -16,17 +15,29 @@ namespace NaukaCSharpDamol
     public partial class MainWindow : Window
     {
         private int timeleft;
+        public static bool isWindow2Active = false;
         private string login, password;
         private static DispatcherTimer Timer1;
-        static string connStr = "server=localhost;user=root;database=tsl;port=3306;password=;";
-        MySqlDataReader rdr;
-        public MySqlConnection conn = new MySqlConnection(connStr);
-
+        private static string connStr = "server=localhost;user=root;database=tsl;port=3306;password=;";
+        public static MySqlDataReader rdr;
+        private static MySqlConnection conn = new MySqlConnection(connStr);
+        private string LoggedAccount;
         public MainWindow()
         {
+            //if (!File.Exists(Directory.GetCurrentDirectory() + "\\MySql.Data.dll"))
+            //{
+            //    MessageBoxResult result = MessageBox.Show("Brak pliku MySql.Data.dll");
+            //    if (result==MessageBoxResult.OK)Environment.Exit(0);
+            //}
+            //if (!File.Exists(Directory.GetCurrentDirectory() + "\\MySql.Web.dll"))
+            //{
+            //    MessageBox.Show("Brak pliku MySql.Web.dll");
+            //}
+
             Window1 Okno = new Window1();
             Okno.Show();
             MySQL_Init(Okno);
+            
 
         }
         private void MySQL_Init(Window1 uchwyt)
@@ -48,16 +59,15 @@ namespace NaukaCSharpDamol
                 Environment.Exit(0);
             }
         }
-        private string MySQL_Query(string query)
+        public static string MySQL_LoginQuery(string query)
         {
-
             try
             {
                 MySqlCommand cmd = new MySqlCommand(query, conn);
                 rdr = cmd.ExecuteReader();
                 if (rdr.Read()) return rdr[0].ToString();
                 else return "";
-                
+
 
             }
             catch (Exception ex)
@@ -70,7 +80,23 @@ namespace NaukaCSharpDamol
                 return "";
 
             }
-            
+        }
+        public static void MySQL_UpdateQuery(string query)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                #if (DEBUG)
+                    MessageBox.Show(ex.ToString(), "DEBUG INFORMATION");
+                    MessageBox.Show(ex.GetType().ToString(), "Error - debug");
+                #endif
+                MessageBox.Show("There was an error when processing your request.", "Error");
+
+            }  
         }
         private void Odliczanie(Object source, EventArgs e)
         {
@@ -99,6 +125,24 @@ namespace NaukaCSharpDamol
             Timer1.Start();
             Button1.IsEnabled = false;
         }
+
+        private void Label_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (!isWindow2Active)
+            {
+                Window2 okno2 = new Window2();
+                isWindow2Active = true;
+            }
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            Button1.IsEnabled = true;
+            LogoutButton.IsEnabled = false;
+            LoggedAs.Content = "---";
+            LoggedAccount = "";
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string DBResult_login, DBResult_password;
@@ -110,7 +154,7 @@ namespace NaukaCSharpDamol
             }
             else
             {
-                DBResult_login = MySQL_Query("SELECT konta.login FROM konta WHERE konta.login = \"" + login + "\";");
+                DBResult_login = MySQL_LoginQuery("SELECT konta.login FROM konta WHERE konta.login = \"" + login + "\";");
                 rdr.Close();
                 if (DBResult_login == "")
                 {
@@ -121,16 +165,25 @@ namespace NaukaCSharpDamol
                 else
                 {
                     password = Encryption.EncryptSHA512Managed(password);
-                    DBResult_password = MySQL_Query("SELECT konta.password FROM konta WHERE konta.login = \"" + login + "\";");
+                    DBResult_password = MySQL_LoginQuery("SELECT konta.password FROM konta WHERE konta.login = \"" + login + "\";");
                     rdr.Close();
                     if (DBResult_password == password && DBResult_login == login)
                     {
+                        MySQL_UpdateQuery("UPDATE `konta` SET `LastLogin` = CURRENT_TIMESTAMP WHERE `konta`.`login` = \""+login+"\";");
                         MessageBox.Show("Zalogowano.", "Informacja");
+                        Button1.IsEnabled = false;
+                        LogoutButton.IsEnabled = true;
+                        LoggedAs.Content = login;
+                        LoggedAccount = login;
+                        TextBoxA.Text = "";
+                        PasswordBox1.Password = "";
                     }
                     else
                     {
                         Start_Odliczania();
-                        MessageBox.Show("Błędne hasło.", "Błąd");  
+                        TextBoxA.Text = "";
+                        PasswordBox1.Password = "";
+                        MessageBox.Show("Błędne hasło.", "Błąd");
                     }
                 }
             }
---
+++ NaukaCSharpDamol/Window2.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+
+namespace NaukaCSharpDamol
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Window2_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MainWindow.isWindow2Active=false;
+        }
+
+        private bool isPasswordStrong(string password)
+        {
+            if(password.Length>=8 && password.Length<=20)
+            {
+                bool tCapital = false, tNormal = false, tNumber = false;
+                for(int i = 0; i < password.Length; i++)
+                {
+                    if (password[i] >= 'A' && password[i] <= 'Z')
+                    {
+                        tCapital = true;
+                    }       
+                    else if (password[i] >= 'a' && password[i] <= 'z')
+                    {
+                        tNormal = true;
+                    }
+                    else if (password[i] >= '0' && password[i] <= '9')
+                    {
+                        tNumber = true;
+                    }
+                }
+                //MessageBox.Show(tCapital.ToString()+tNormal.ToString()+tNumber.ToString());
+                if (tCapital == true && tNormal == true && tNumber == true)
+                    return true;
+                else return false;
+            }
+            else return false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = Login_Window2.Text;
+            string password = Password_Window2.Password;
+            if (login != "" && password != "" && PasswordRepeat_Windows2.Password != "")
+            {
+                string DBResult_login = MainWindow.MySQL_LoginQuery("SELECT konta.login FROM konta WHERE konta.login = \"" + login + "\";");
+                MainWindow.rdr.Close();
+                if (DBResult_login.ToLower() != login.ToLower())
+                {
+                    if (password == PasswordRepeat_Windows2.Password)
+                    {
+                        if (isPasswordStrong(password))
+                        {
+                            if (RulesCheck_Windows2.IsChecked == true)
+                            {
+                                try
+                                {
+                                    MainWindow.MySQL_UpdateQuery("INSERT INTO `konta` (`id`, `login`, `password`, `DateRegistered`, `LastLogin`) VALUES(NULL, '" + Login_Window2.Text + "', '" + Encryption.EncryptSHA512Managed(Password_Window2.Password) + "', CURRENT_TIMESTAMP, '')");
+                                    MessageBox.Show("Zarejestrowano pomyślnie.", "Informacja");
+                                }
+                                catch(Exception ex)
+                                {
+                                    #if (DEBUG)
+                                        MessageBox.Show(ex.ToString(), "DEBUG INFORMATION");
+                                        MessageBox.Show(ex.GetType().ToString(), "Error - debug");
+                                    #endif
+                                    MessageBox.Show("Wystąpił nieznany błąd, skontaktuj się z administratorem.", "Error");
+                                }
+                            }
+                            else
+                            {
+                                MessageBox.Show("Musisz zaakceptować regulamin.", "Błąd");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Hasło nie spełnia wymogów. Hasło musi posiadać 8-20 znaków, przynajmniej jedną cyfrę, jedną małą oraz jedną dużą literę.", "Błąd");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Podane hasła nie zgadzają się.", "Błąd");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Konto o takim loginie już istnieje.", "Błąd");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Żadne z pól nie może pozostać puste.", "Błąd");
+            }
+        }
+    }
+}
